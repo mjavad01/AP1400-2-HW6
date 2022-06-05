9aab2f8ae18991cb4c5798f18804d709d6e4d2a2
mjavad01 <j.zeinoddini@gmail.com>
2022-06-05 21:39:11 +0430

    q2 finished

--- include/q2.h
+++ include/q2.h
@@ -10,57 +10,67 @@
 
 namespace q2 {
 
-struct Patient {
-    Patient(std::string _name, size_t _age, size_t _smokes, size_t _area_q, size_t _alkhol)
-        : name { _name }
-        , age { _age }
-        , smokes { _smokes }
-        , area_q { _area_q }
-        , alkhol { _alkhol } {};
+    struct Patient
+    {
+    	std::string name;
+    	size_t age;
+    	size_t smokes;
+    	size_t area_q;
+    	size_t alkhol;
+    };
 
-    std::string name;
-    size_t age;
-    size_t smokes;
-    size_t area_q;
-    size_t alkhol;
-};
 
-std::vector<Patient> read_file(std::string filename)
-{
-    std::ifstream file(filename);
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string txt = buffer.str();
-
-    std::regex pattern(R"((\w+)\ ?,(\w+)\ ?,(\d+)\,(\d+)\,(\d+)\,(\d+))");
-    std::smatch match;
-
-    std::vector<Patient> Patients {};
-
-    while (std::regex_search(txt, match, pattern)) {
-        std::string first_name { match[1] }, last_name { match[2] };
-        std::string full_name { first_name + " " + last_name };
-        // I use std::stoi for converting string to integer
-        size_t age { static_cast<size_t>(std::stoi(match[3])) };
-        size_t smokes { static_cast<size_t>(std::stoi(match[4])) };
-        size_t area_q { static_cast<size_t>(std::stoi(match[5])) };
-        size_t alkhol { static_cast<size_t>(std::stoi(match[6])) };
-        Patients.push_back(Patient { full_name, age, smokes, area_q, alkhol });
-        txt = match.suffix().str();
+    std::vector<Patient> read_csv(const std::string& filename)
+    {
+        std::vector<Patient> result {};
+        std::ifstream csv_file(filename);
+        std::string line {};
+        std::regex rgx(R"((\w+)\s*,(\w+)\s*,(\d+),(\d+),(\d+),(\d+)\s*)");
+        std::smatch match;
+        if (csv_file.is_open())
+        {
+            while (getline(csv_file, line))
+            {
+                Patient p{};
+                if (std::regex_match(line, match, rgx))
+                 {
+                    p.name = std::string(match[1]) + " " + std::string(match[2]);
+                    p.age = std::stoi(match[3]);
+                    p.smokes = std::stoi(match[4]);
+                    p.area_q = std::stoi(match[5]);
+                    p.alkhol = std::stoi(match[6]);
+                    result.push_back(p);
+                }
+            result.push_back(p);
+            }
+        csv_file.close();
+        }
+    return result;
     }
 
-    return Patients;
-}
+    // display patients vector in csv format
+    void display_csv(std::vector<Patient>& patients)
+    {
+        for (auto& p : patients)
+        {
+            std::cout << p.name << "," << p.age << "," << p.smokes << "," << p.area_q << "," << p.alkhol << std::endl;
+        }
+    }
 
-bool comparison(Patient& a, Patient& b)
-{
-    return 3 * (a.age) + 5 * (a.smokes) + 2 * (a.area_q) + 4 * (a.alkhol) > 3 * (b.age) + 5 * (b.smokes) + 2 * (b.area_q) + 4 * (b.alkhol);
-}
 
-void sort(std::vector<Patient>& patients)
-{
-    std::sort(patients.begin(), patients.end(), comparison);
-}
+    bool compare(Patient& a, Patient& b)
+    {
+        size_t cancer_probaplity_a {3*a.age + 5*a.smokes + 2*a.area_q + 4*a.alkhol};
+        size_t cancer_probaplity_b{3*b.age + 5*b.smokes + 2*b.area_q + 4*b.alkhol};
+        
+        return cancer_probaplity_a > cancer_probaplity_b;
+    }
+
+    void sort(std::vector<Patient>& patients)
+    {
+        std::sort(patients.begin(), patients.end(), compare);
+    }
+
 }
 
 #endif // Q2_H
